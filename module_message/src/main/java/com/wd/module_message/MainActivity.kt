f2ff88477be58de948067742da3a9eea079721f5
张耀中 <749903998@qq.com>
2019-04-12 13:08:42 +0800

    架构

---
+++ module_message/src/main/java/com/wd/module_message/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.wd.module_message
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.wd.module_information.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
